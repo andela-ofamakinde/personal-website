b2e156706148a4a5f8a190cfc501d062a4a34366
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2019-03-25 23:53:18 +0100

    Add sigin and signout buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -11,17 +11,38 @@ class GoogleAuth extends Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        this.auth.isSignedIn.listen(this.onAuthChange)
       })
     })
   };
   
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+  };
+  
+  onSignIn = () => {
+    this.auth.signIn()
+  };
+  
+  onSignOut = () => {
+    this.auth.signOut()
+  };
+  
   renderAuthButton() {
     if(this.state.isSignedIn === null) {
-      return <div>Signin Status Unknown</div>
+      return null
     } else if(this.state.isSignedIn) {
-      return <div>Signed in</div>
+      return (
+        <button onClick={this.onSignOut} className='ui red google button'>
+          <i className="google icon" />Sign Out
+        </button>
+      )
     } else {
-      return <div>Not Signed in</div>
+      return (
+        <button onClick={this.onSignIn} className='ui red google button'>
+          <i className="google icon" />Sign In
+        </button>
+      )
     }
   }
   
